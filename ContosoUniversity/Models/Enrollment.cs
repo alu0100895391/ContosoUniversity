9c1c0ca4925b30934b23df638d0b8dde7a60d4fb
alu0100895391 <alu0100895391@ull.edu.es>
2019-03-24 13:39:37 +0000

    Crear un modelo de datos complejo(modificar la entidad Enrollment)

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -8,7 +8,28 @@
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
-        public int CourseID { get; set; }
+        public int CourseID {using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+    {
+        public enum Grade
+        {
+            A, B, C, D, F
+        }
+
+        public class Enrollment
+        {
+            public int EnrollmentID { get; set; }
+            public int CourseID { get; set; }
+            public int StudentID { get; set; }
+            [DisplayFormat(NullDisplayText = "No grade")]
+            public Grade? Grade { get; set; }
+
+            public virtual Course Course { get; set; }
+            public virtual Student Student { get; set; }
+        }
+    }
+    get; set; }
         public int StudentID { get; set; }
         public Grade? Grade { get; set; }
 
